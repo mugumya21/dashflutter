68306d8dda8fa66c36a9d4ccfa471d59c098c826
mugumya21 <mugumyavicent21@gmail.com>
2023-11-12 16:23:20 -0800

    Initial commit

---
+++ lib/layout.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp( const SiteLayout());
+
+}
+
+class SiteLayout extends StatelessWidget {
+
+  const SiteLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: Row(
+        children: [
+          Expanded(child: Container(
+            color: Colors.red,
+          )),
+          Expanded(
+              flex: 5,
+              child: Container(
+                  color: Colors.blue
+              ))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart'; // Correct import for GetMaterialApp
+import 'package:google_fonts/google_fonts.dart';
+
+import './layout.dart'; // Replace 'your_package_name' with the actual package name
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context){
+    return GetMaterialApp(
+      debugShowCheckedModeBanner : false,
+      title: "Dash",
+      theme : ThemeData(
+          scaffoldBackgroundColor : Colors.white,
+          textTheme : GoogleFonts.mulishTextTheme(Theme.of(context).textTheme ).apply(
+              bodyColor : Colors.black
+          ),
+          pageTransitionsTheme : const PageTransitionsTheme(builders: {
+            TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
+            TargetPlatform.android : FadeUpwardsPageTransitionsBuilder()
+          }),
+          primaryColor : Colors.blue
+      ),
+      home : const SiteLayout(),
+    );
+  }
+}
